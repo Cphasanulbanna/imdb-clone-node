b25803845b275cead34c2805a272427c55a0f6a5
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-07-02 11:44:18 +0530

    pagination bug fixed

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,7 +3,7 @@ const movies = require("../movies.json");
 
 const getMovies = async (req, res) => {
     try {
-        const page = req.query.search ? 0 : parseInt(req.query.page) - 1 || 0;
+        const page = parseInt(req.query.page) - 1 || 0;
         const limit = parseInt(req.query.limit) || 5;
         const search = req.query.search || "";
         let sort = req.query.sort || "rating";
